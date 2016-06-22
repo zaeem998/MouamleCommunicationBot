b68e7046d7fffdee5786dc02efe3ab422bdd4362
mouamle <mouamle99@bk.ru>
2016-06-22 15:53:36 +0000

    First commit

---
+++ config.lua
@@ -0,0 +1,21 @@
+-- @Mouamle   @IlulI
+return 
+{
+	
+	MToken = "", -- تعال لأي معرف من المعرفات الفوك وشوف شراح يطلب مننك وينطيك رقم تحطه هنا
+	
+	token = "", -- توكن البوت تجيبه من بوت فاذر
+	username = "", -- معرف البوت
+	yourId = "", -- الايدي مالتك
+	groupId = "-", -- ايدي الكروب اللي حتضيف بي البوت حتى اكثر من واحد يردون
+	BanMessage = "*You were banned*", -- الرساله اللي تطلع للمستخدم من ينحضر
+	unBanMessage = "*You were released*", -- الرساله اللي تطلع للمستخدم من ينفك الحضر عننه
+	
+	WelcomeMessage =  -- رسالة الترحيب
+	[[
+	*Welcome* i'm a CommunicationBot
+	`I will send the messages back and forth between you and my owner`
+	]],
+	SentNotification = "Message Sent" -- الرساله اللي ترجع للمستخدم من يدز رساله للبوت
+}
+
